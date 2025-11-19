6b908ad4e2a03a934a01cc864535c9265120e34b
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-11-19 05:08:39 +0000

    video conflict rersolve

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -1,11 +1,7 @@
 import mv from '/src/assets/videos/video'
 export default function VideoSection(){
     return(
-<<<<<<< HEAD
-        <div className="lg:h-screen w-full">
-=======
         <div className="xl:h-screen w-full">
->>>>>>> e569bce99344326b3e65f2d8d8f0d16b34d5352d
             <video 
                 src={mv.main}
                 preload='auto'
